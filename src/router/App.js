806b585a0285b3d1f25252f83e04742a42701018
pabloIO <pabloIO>
2019-05-04 17:19:55 -0400

    validation and ready to send form with indexed answers

--- src/router/App.js
+++ src/router/App.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
-import '../App.css';
+// import '../App.css';
+import './style.css'
 import  questions from '../assets/questions';
-import { Container, Row, Col, Form, Button, Collapse } from 'react-bootstrap';
-		// {
+import { 
+    Container, Row, Col, 
+    Form, Button, Collapse, 
+    Fade, Jumbotron, Alert,
+    Figure
+} from 'react-bootstrap';
+import { BarChart  } from 'react-chartkick'
+import Chart from 'chart.js'
+// {
 		// 	"question": "11. Elija la forma correcta de ingresar una imagen?",
 		// 	"answers": ["<img src='foto.jpg'>***", "<imagen src='foto.jpg'>", "<img href='foto.jpg'>"],
 		// 	"index_correct_answer": 0
@@ -86,26 +94,37 @@ import { Container, Row, Col, Form, Button, Collapse } from 'react-bootstrap';
 
 const Questions = props => {
     return(
-        questions[props.type].map((e, i) => {
-            return(
-                <Col 
-                    key={i} md={12}>
-                    <h4 key={i}>{e.question}</h4>
-                    {
-                        e.answers.map((answer, j) => {
-                            return(
-                                <Form.Check 
-                                    key={j}
-                                    type={'radio'}
-                                    id={`default-radio`}
-                                    label={answer}
-                                />
-                            );
-                        })
-                    }
-                </Col>
-            );
-        })
+        <>
+        <div className={`title-container ${props.type.toLowerCase()}-title`}>
+            <h2 className="title">{props.type}</h2>
+        </div>
+        <>
+        {
+            props.data.map((e, i) => {
+                return(
+                    <Col 
+                        key={i} md={12}>
+                        <h4 key={i}>{e.question}</h4>
+                        {
+                            e.answers.map((answer, j) => {
+                                return(
+                                    <Form.Check 
+                                        checked={j == e.checked}
+                                        key={j}
+                                        type={'radio'}
+                                        id={`default-radio`}
+                                        label={answer}
+                                        onChange={() => props._checkField(props.type, i, j)}
+                                    />
+                                );
+                            })
+                        }
+                    </Col>
+                );
+            })
+        }
+        </>
+        </>
     );
 }
 
@@ -114,9 +133,123 @@ class App extends Component {
         super(props, context);
 
         this.state = {
+            alias: '',
+            email: '',
+            name: '',
+            gender: null,
             showForm: false,
+            quizStarted: false,
+            isLoadingSuccessMsgError: null,
             isSubmitted: false,
+            results: [],
+            HTML: questions['HTML'].map((e, i) => { return{...e, checked: null} }),
+            CSS: questions['CSS'].map((e, i) => { return{...e, checked: null} }),
+            JS: questions['JS'].map((e, i) => { return{...e, checked: null} }),
+            isLoading: false,
+            isLoadingSuccess: true,
+            isLoadingSuccessMsg: '',
         };
+    }
+
+    checkField(type, indexAnswer, indexOption){
+        // copy type questionare
+        const copy = this.state[type];
+        // update right index checking field 'checked' as the index of the option chosen
+        const updated = copy.map((e, i) => {
+            if(indexAnswer === i){
+                return{
+                    ...e, checked: indexOption
+                }
+            }
+            return e;
+        });
+        this.setState({[type]: updated});
+    }
+
+    fillForm({target}){
+        this.setState({
+            [target.name]: target.value
+        });
+    }
+
+    startTest(){
+        const { showForm, email, name, gender } = this.state;
+        if(!this.validatePersonalData()){
+            this.setState({
+                isLoading: false,
+                isLoadingSuccess: false,
+                isLoadingSuccessMsg: 'Debe llenar todos los campos',
+                isLoadingSuccessMsgError: 'warning',
+            });
+            return;
+        }
+        this.setState({showForm: true, quizStarted: true});
+        this.startCounter();
+    }
+
+    validatePersonalData(){
+        const { email, name, gender } = this.state;
+        return !(email.trim() == '' || name.trim() == '' || !gender);
+    }
+    /**
+     * @param min: Minimun value accepted to submit
+     * @param max: Max value accepted to submit
+     * @param n: Number to normalize
+     */
+    normalize(n, min=0, max=30){
+        console.log(n)
+        const norm = (n - min) / (max - min);
+        console.log(norm)
+        return norm;
+    }
+
+    getResults(){
+        const { CSS, HTML, JS } = this.state;
+        const cssCorrect = CSS.filter(e => e.checked == e.index_correct_answer);
+        const htmlCorrect = HTML.filter(e => e.checked == e.index_correct_answer);
+        const jsCorrect = JS.filter(e => e.checked == e.index_correct_answer);
+        const sum = cssCorrect.length + htmlCorrect.length + jsCorrect.length;
+        console.log(cssCorrect, htmlCorrect)
+        return {
+            css: cssCorrect, 
+            html: htmlCorrect,
+            js: jsCorrect,
+            score: this.normalize(sum)
+        };        
+    }
+
+    startCounter(){}
+
+    submit(){
+        const { email, name, gender, CSS, HTML, } = this.state;
+        if(!this.validatePersonalData()){
+            this.setState({
+                isLoading: false,
+                isLoadingSuccess: false,
+                isLoadingSuccessMsg: 'Debe llenar todos los campos',
+                isLoadingSuccessMsgError: 'warning',
+            });
+            return;
+        }
+        const data = this.getResults();
+        console.log(data);
+        this.setState({
+            isLoading: true
+        });
+
+        if(true){
+            this.setState({
+                isLoading: false,
+                isLoadingSuccess: true,
+                isLoadingSuccessMsgError: 'success',
+                isLoadingSuccessMsg: 'Su formulario fue enviado con éxito, debajo podrá ver sus resultados',
+                isSubmitted: true,
+                results: {
+                    data: [['CSS', data.css.length], ['HTML', data.html.length]],
+                    score: data.score
+                }
+            });
+        }
     }
 
     render(){
@@ -128,54 +261,129 @@ class App extends Component {
                             <p style={{color: '#fff'}}>PUBLICIDAD PARA EL EVENTO</p>
                         </Col>
                         <Col md={8} style={{backgroundColor: '#f9f9f9'}}>
-                            <h1> CUESTIONARIO</h1>  
-                            <h2> REACT BOLIVIA WORKSHOP</h2>  
+                            <Jumbotron className="jumbotron-container" fluid style={{padding: 30}}>
+                                <h1 style={{textAlign: 'center', color: '#fff'}}> React Bolivia Workshop</h1>  
+                                <p style={{color: '#fff'}}>
+                                    En esta ocasión la comunidad organiza un <span style={{fontWeight: 'bold'}}>taller práctico</span>
+                                    en el que seleccionaremos solamente a las <span style={{fontWeight: 'bold'}}>30 mejores postulaciones</span>. <br/>
+                                    Esto es debido a que tenemos espacio limitado para 30 personas, en los ambientes de la Universidad Del Valle que
+                                    es nuestra auspiciadora en esta ocasión, por favor llene todos los campos.
+                                </p>
+                            </Jumbotron>
                             <Form>
                                 <Form.Group controlId="formName">
                                     <Form.Label>Nombre y apellidos</Form.Label>
-                                    <Form.Control type="email" placeholder="Tu nombre completo" />
+                                    <Form.Control 
+                                        name="name"
+                                        value={this.state.name}
+                                        onChange={this.fillForm.bind(this)}
+                                        type="text" 
+                                        placeholder="Tu nombre completo" />
+                                    <Form.Text className="text-muted" >
+                                        Se usarán las iniciales de tu nombre para publicar una lista de las notas. 
+                                    </Form.Text>
                                 </Form.Group>
                                 
                                 <Form.Group controlId="formBasicEmail">
                                     <Form.Label>Correo electrónico</Form.Label>
-                                    <Form.Control type="email" placeholder="Tu correo electrónico" />
+                                    <Form.Control 
+                                        name="email"
+                                        value={this.state.email}
+                                        onChange={this.fillForm.bind(this)}
+                                        type="email" 
+                                        placeholder="Tu correo electrónico" />
                                     <Form.Text className="text-muted" >
                                         Verifica que tu correo electrónico sea el correcto, pues lo utilizaremos para contactarte y mandarte contenido promocional.
                                     </Form.Text>
                                 </Form.Group>
                                 <Form.Group controlId="formBasicEmail">
                                     <Form.Label>Género</Form.Label> <br/>
-                                    <Form.Check inline label="Masculino" type={'radio'} id={`inline-radio-1`} />
-                                    <Form.Check inline label="Femenino" type={'radio'} id={`inline-radio-2`} />
+                                    <Form.Check 
+                                        onChange={() => this.setState({gender: 'masculino'})}
+                                        checked={this.state.gender == 'masculino'}
+                                        inline label="Masculino" type={'radio'} id={`inline-radio-1`} />
+                                    <Form.Check                                         
+                                        onChange={() => this.setState({gender: 'femenino'})}
+                                        checked={this.state.gender == 'femenino'}
+                                        inline label="Femenino" type={'radio'} id={`inline-radio-2`} />
                                     <Form.Text className="text-muted" >
                                         Solo puedes llenar el cuestionario una vez, si demostramos que lo hiciste más de una vez, eliminaremos tu postulación. <br/>
                                         Tendrás 15 minutos para resolver todo el cuestionario, buena suerte 😉
                                     </Form.Text>
                                 </Form.Group>
                                 <Button 
-                                    onClick={() => this.setState({showForm: true})}
+                                    onClick={() => this.startTest()}
                                     variant="outline-success" size="lg" block style={{marginTop: 50, marginBottom: 25}}>
                                     Entiendo, empezar
                                 </Button>
                                 <Collapse in={this.state.showForm}>
                                     <div id="example-collapse-text">
                                         <Questions
+                                            _checkField={this.checkField.bind(this)}
+                                            data={this.state.HTML}
                                             type="HTML"/>
                                         <Questions
+                                            _checkField={this.checkField.bind(this)}
+                                            data={this.state.CSS}
                                             type="CSS"/>
+                                        <Questions
+                                            _checkField={this.checkField.bind(this)}
+                                            data={this.state.JS}
+                                            type="JS"/>
                                     </div>
                                 </Collapse>
-
+                                {
+                                    !this.state.isLoadingSuccess && 
+                                    <Fade in={!this.state.isLoadingSuccess}>
+                                        <Alert style={{fontWeight: 'bold', textAlign: 'center'}} variant={this.state.isLoadingSuccessMsgError}>
+                                            {this.state.isLoadingSuccessMsg}
+                                        </Alert>
+                                    </Fade>
+                                }
+                                {   
+                                    this.state.isSubmitted &&
+                                    <Fade in={this.state.isSubmitted}>
+                                        <Alert style={{fontWeight: 'bold', textAlign: 'center'}} variant={this.state.isLoadingSuccessMsgError}>
+                                            {this.state.isLoadingSuccessMsg}
+                                        </Alert>
+                                    </Fade>
+                                }
                                 <Button 
-                                    style={{marginTop: 25, marginBottom: 50}}
-                                    disabled={!this.state.isSubmitted}
+                                    className="submit"
+                                    style={{marginTop: 25, marginBottom: 50, }}
+                                    disabled={this.state.isLoading || !this.state.quizStarted}
                                     variant="outline-primary" 
-                                    type="submit"
-                                    size="lg" block >
-                                    Enviar
+                                    size="lg" block 
+                                    onClick={() => this.submit()}>
+                                    { this.state.isLoading ? 'Subiendo...' : 'Enviar' }
                                 </Button>
                             </Form>
+                            { this.state.isSubmitted && 
+                            <Col>
+                                <Fade
+                                    timeout={1000} 
+                                    in={!this.state.isSubmitted}>
+                                    <div id="example-collapse-text">
+                                        <Figure>
+                                            <Figure.Image
+                                                rounded
+                                                width={150}
+                                                height={150}
+                                            />
+                                            <Figure.Caption>
+                                                { this.state.results.score }
+                                            </Figure.Caption>
+                                        </Figure>;
+                                        <BarChart 
+                                            colors={["#EF476F", "#FF5722", "#4CD964"]}
+                                            min={0} max={30}
+                                            data={this.state.results.data} />
+                                    </div>
+                                </Fade>
+                            </Col>
+                            }
                         </Col>
+                        
                     </Row>
                 </Container>
             </div>
